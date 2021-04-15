aa1d9c63bdc380712e253b23b741716effbd5df9
Rijn Buve <rijn@buve.nl>
2021-04-15 12:23:38 +0200

    Updated copyright

--- lbs/src/main/java/com/tomtom/speedtools/services/lbs/route/RouteEngineResponse.java
+++ lbs/src/main/java/com/tomtom/speedtools/services/lbs/route/RouteEngineResponse.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2019, TomTom (http://tomtom.com).
+ * Copyright (C) 2012-2021, TomTom (http://tomtom.com).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -84,4 +84,3 @@ public final class RouteEngineResponse implements JsonRenderable {
         return Json.toStringJson(this);
     }
 }
-
